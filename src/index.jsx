4361967ab53591bac536850ab295d5f1d8c5def9
Linda <linda.tamakaha@gmail.com>
2018-11-09 14:32:13 +0000

    first commit

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,5 @@
 import 'raf/polyfill';
 import React from 'react';
 import { render } from 'react-dom';
-import App from './components/app';
 
-render(<App />, document.getElementById('root'));
+render(<h1>One Page API</h1>, document.getElementById('root'));
